7c07b95b1a3b5cac7bcdfb706ac21141f77cf1d6
Panos Lantavos-Stratigakis <panoslantavos@gmail.com>
2022-02-14 15:30:04 +0200

    Initialize user gitconfig

--- src/files.tf
+++ src/files.tf
@@ -4,10 +4,26 @@ locals {
     ".bash_aliases",
     ".config/starship.toml",
   ]
+
+
+  files_templated = [
+    ".gitconfig",
+  ]
+
+  template_vars = {
+    EMAIL    = var.email
+    NAME     = var.name
+  }
 }
 
 resource "local_file" "files" {
   for_each = toset(local.files)
   content  = file("${path.module}/files/${each.value}")
+  filename = pathexpand("~/${each.value}")
+}
+
+resource "local_file" "template_file" {
+  for_each = toset(local.files_templated)
+  content  = templatefile("${path.module}/files-templated/${each.value}", local.template_vars)
   filename = pathexpand("~/${each.value}")
 }
--- src/modules.tf
+++ src/modules.tf
@@ -1,3 +1,7 @@
 module "dnf" {
   source = "./dnf"
 }
+
+module "binary" {
+  source = "./binary"
+}
