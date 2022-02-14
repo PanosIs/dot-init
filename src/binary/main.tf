cc8f207c482b95c9d664bc1e92f7902a4b7829ce
Panos Lantavos-Stratigakis <panoslantavos@gmail.com>
2022-02-14 15:29:17 +0200

    Add binary module with Obsidian install

---
+++ src/binary/main.tf
@@ -0,0 +1,23 @@
+locals {
+  binary_list = [
+    "obsidian",
+  ]
+
+  binary_versions = {
+    obsidian = "0.13.23"
+  }
+}
+
+resource "shell_script" "obsidian" {
+  for_each = toset(local.binary_list)
+
+  lifecycle_commands {
+    create = file(format("${path.root}/scripts/install-%s.sh", each.value))
+    read   = "echo \"{\"path\": \"$(which obsidian)\"}\""
+    delete = "sudo rm $(which obsidian)"
+  }
+
+  environment = {
+    VERSION = local.binary_versions[each.value]
+  }
+}
